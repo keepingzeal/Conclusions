38f78f374a310c7473e363b3d5fcf2e81678f3ff
kzeal <274618323@qq.com>
2019-08-27 08:32:42 +0800

    8/26

---
+++ php_other/php20190826.php
@@ -0,0 +1,41 @@
+<?php
+
+// 直角三角形
+// for ($i=0; $i < 10; $i++) { 
+// 	echo str_repeat('*', $i).'<br>';
+// }
+
+// echo '<br>';
+// 等腰三角形
+$max = 10;
+for ($i=0; $i < $max; $i++) { 
+	for ($j=$max; $j > $i ; $j--) { 
+		echo '&nbsp;';
+	}
+	for ($k=0; $k < ($i + 1) * 2 - 1; $k++) { 
+		echo '*';
+	}
+	echo '<br>';
+}
+
+echo '<br>';
+
+for ($i=0; $i < $max; $i++) { 
+	for ($j=$max; $j > $i ; $j--) { 
+		echo '&nbsp;';
+	}
+	for ($k=0; $k < ($i + 1) * 2 - 1; $k++) {
+		if ($k == 0 || $k == ($i + 1) * 2 - 2) {
+			echo '*';
+		} else {
+			echo '&nbsp;';
+		}
+	}
+	echo '<br>';
+}
+
+
+
+
+
+
