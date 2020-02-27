8e6b4704bf042b73d9ea668496fad1ca1fa82fe5
kzeal <274618323@qq.com>
2020-02-27 21:04:03 +0800

    -m..

--- php_other/php20190826.php
+++ php_other/php20190826.php
@@ -1,5 +1,7 @@
 <?php
 
+echo 123;
+
 // 直角三角形
 // for ($i=0; $i < 10; $i++) { 
 // 	echo str_repeat('*', $i).'<br>';
@@ -7,32 +9,38 @@
 
 // echo '<br>';
 // 等腰三角形
-$max = 10;
-for ($i=0; $i < $max; $i++) { 
-	for ($j=$max; $j > $i ; $j--) { 
-		echo '&nbsp;';
-	}
-	for ($k=0; $k < ($i + 1) * 2 - 1; $k++) { 
-		echo '*';
-	}
-	echo '<br>';
-}
+// $max = 10;
+// for ($i=0; $i < $max; $i++) { 
+// 	for ($j=$max; $j > $i ; $j--) { 
+// 		echo '&nbsp;';
+// 	}
+// 	for ($k=0; $k < ($i + 1) * 2 - 1; $k++) { 
+// 		echo '*';
+// 	}
+// 	echo '<br>';
+// }
 
-echo '<br>';
+// echo '<br>';
 
-for ($i=0; $i < $max; $i++) { 
-	for ($j=$max; $j > $i ; $j--) { 
-		echo '&nbsp;';
-	}
-	for ($k=0; $k < ($i + 1) * 2 - 1; $k++) {
-		if ($k == 0 || $k == ($i + 1) * 2 - 2) {
-			echo '*';
-		} else {
-			echo '&nbsp;';
-		}
-	}
-	echo '<br>';
-}
+// for ($i=0; $i < $max; $i++) { 
+// 	for ($j=$max; $j > $i ; $j--) { 
+// 		echo '&nbsp;';
+// 	}
+// 	for ($k=0; $k < ($i + 1) * 2 - 1; $k++) {
+// 		if ($k == 0 || $k == ($i + 1) * 2 - 2) {
+// 			echo '*';
+// 		} else {
+// 			echo '&nbsp;';
+// 		}
+// 	}
+// 	echo '<br>';
+// }
+
+
+// $str = "123456";
+// $num = 1;
+// $str{--$num} = 99;
+// echo $str;
 
 
 
