12aed5028e7f3ec75f3b548330b43ba6ae10b3a2
kzeal <274618323@qq.com>
2020-12-26 15:42:27 +0800

    无更新

--- php/数组/数组练习.php
+++ php/数组/数组练习.php
@@ -10,3 +10,11 @@ $a2=array("e"=>"red","f"=>"green","g"=>"blue");
 $result=array_diff($a1,$a2);
 print_r($result);
 
+$arr = [
+	'1A' => 123,
+	'1'	=> 321,
+	2.8	=> 8222,
+	false	=> 8222,
+	1	=> 222,
+];
+print_r($arr);
